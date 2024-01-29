0ca29009e4a9cf460c6d5388ff3a6478545d3e2f
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-29 15:05:18 +0100

    #9: Add http requests to backend endpoints

--- src/views/CreateAccountView.vue
+++ src/views/CreateAccountView.vue
@@ -3,6 +3,10 @@ import router from '@/router';
 import SharedView from './SharedView.vue';
 import { useForm } from 'vee-validate';
 import * as yup from 'yup'
+import { useAxios } from '@vueuse/integrations/useAxios'
+import { postMethod, createAccountUrl } from '@/api/requests'
+
+const { data, isLoading, error, execute } = useAxios()
 
 const { errors, handleSubmit, defineField } = useForm({
     validationSchema: yup.object({
@@ -13,8 +17,12 @@ const { errors, handleSubmit, defineField } = useForm({
 const [username] = defineField('username');
 const [fullName] = defineField('fullName');
 
-const onSubmit = handleSubmit(values => {
-
+const onSubmit = handleSubmit(async () => {
+    await execute(createAccountUrl,
+        {
+            method: postMethod,
+            data: { userName: username.value, fullName: fullName.value }
+        })
 })
 
 const redirect = (name: string) => {
@@ -28,25 +36,34 @@ const redirect = (name: string) => {
                 <div class="row gy-3 overflow-hidden">
                     <div class="col-12">
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Username" v-model="username" />
+                            <input class="form-control" placeholder="Username" v-model="username" :disabled="isLoading" />
                             <p v-if="errors.username" class="text-warning">{{ errors.username }}</p>
                             <label for="Username" class="form-label">Username</label>
                         </div>
                     </div>
                     <div class="col-12">
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Full name" v-model="fullName" />
+                            <input class="form-control" placeholder="Full name" v-model="fullName" :disabled="isLoading" />
                             <p v-if="errors.fullName" class="text-warning">{{ errors.fullName }}</p>
                             <label for="Full-name" class="form-label">Full name</label>
                         </div>
                     </div>
                     <div class="col-12">
                         <div class="d-grid">
-                            <button class="btn btn-dark btn-lg" type="submit">Create</button>
+                            <button class="btn btn-dark btn-lg" type="submit" :disabled="isLoading">Create</button>
                         </div>
                     </div>
                 </div>
             </form>
+            <div v-if="isLoading">
+                LOADING
+            </div>
+            <div v-if="error">
+                <p class="text-danger">Error creating an account</p>
+            </div>
+            <div v-if="data">
+                <p class="text-primary"> {{ data }}</p>
+            </div>
             <div class="row">
                 <div class="col-12">
                     <div class="d-flex gap-1 flex-column justify-content-start mt-3">
