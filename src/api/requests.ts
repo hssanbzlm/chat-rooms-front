75593e503f537bcad177ad5d06d3f82d23656771
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-30 17:01:19 +0100

    #11: Add auth guard

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,5 +1,7 @@
 const baseUrl = import.meta.env.VITE_API_URL
 export const postMethod = 'POST'
+export const getMethod = 'GET'
 export const createAccountUrl = `${baseUrl}/api/user/add`
 export const createRoomUrl = `${baseUrl}/api/chat-room/create`
 export const joinRoomUrl = `${baseUrl}/api/auth/join`
+export const isAuthUrl = `${baseUrl}/api/auth/is-auth`
