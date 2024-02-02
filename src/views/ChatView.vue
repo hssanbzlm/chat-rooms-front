69a811097039cea8b741a685ddf631087d041219
Hssan Bouzlima <bouzlima.h@live.fr>
2024-02-02 16:42:26 +0100

    #17 Enhance UI and remove useless html and css (#24)

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -20,78 +20,45 @@ const sendMessage = (message: string) => {
 
 <template>
   <div class="container mt-2">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card mb-4">
-          <div class="people-list mt-2 p-3">
-            <ChatInput size="xl" icon="magnifying-glass" placeholder="Search ...." />
-            <ul class="chat-list mt-4 mb-0">
-              <ChatProfile v-for="user in connectedUsers" :key="user._id"
-                :img="'https://bootdey.com/img/Content/avatar/avatar1.png'" :name="user.fullName" :status="'connected'" />
-
-            </ul>
-          </div>
-          <div class="chat">
-            <ChatHeader :chatName="'First chat'" :created="'10/10/2024'" />
-            <div v-if="isLoading"> LOADING</div>
-            <div v-else-if="error">
-              ERROR
-            </div>
-            <div v-else class="chat-history p-3">
-              <ul>
-                <ChatMessage v-for="message in messages" :key="message._id" :isMyMessage="message.isMyMessage"
-                  :time="message.date.toString()" :img="'https://bootdey.com/img/Content/avatar/avatar7.png'"
-                  :message="message.content" :fullName="message.isMyMessage ? 'You' : message.sender.fullName" />
-              </ul>
-            </div>
-            <div class="chat-message mb-1 p-1">
-              <ChatInput size="xl" icon="paper-plane" placeholder="Enter text here..." @send-message="sendMessage" />
-            </div>
-          </div>
+    <div class="card shadow-lg mb-4 d-flex flex-row">
+      <div class="people-list mt-2 p-3 d-none d-md-inline ">
+        <ChatInput size="xl" icon="magnifying-glass" placeholder="Search ...." />
+        <div class="chat-list overflow-auto mt-4 mb-0">
+          <ChatProfile v-for="user in connectedUsers" :key="user._id"
+            :img="'https://bootdey.com/img/Content/avatar/avatar1.png'" :name="user.fullName" :status="'connected'" />
+        </div>
+      </div>
+      <div class="chat w-100">
+        <ChatHeader :chatName="'First chat'" :created="'10/10/2024'" />
+        <div v-if="isLoading"> LOADING</div>
+        <div v-else-if="error">
+          ERROR
+        </div>
+        <div v-else class="chat-history overflow-auto p-3">
+          <ul>
+            <ChatMessage v-for="message in messages" :key="message._id" :isMyMessage="message.isMyMessage"
+              :time="message.date.toString()" :img="'https://bootdey.com/img/Content/avatar/avatar7.png'"
+              :message="message.content" :fullName="message.isMyMessage ? 'You' : message.sender.fullName" />
+          </ul>
+        </div>
+        <div class="mb-1 p-1">
+          <ChatInput size="xl" icon="paper-plane" placeholder="Enter text here..." @send-message="sendMessage" />
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-.card {
-  box-shadow: 0 10px 10px 0 rgb(0 0 0 / 50%);
-}
-
 .people-list {
-  width: 280px;
-  position: absolute;
-  left: 0;
-  top: 0;
+  width: 300px;
 }
 
-.chat {
-  margin-left: 280px;
+.chat-history,
+.chat-list {
+  height: 75vh;
 }
 
-@media only screen and (max-width: 767px) {
-  .people-list {
-    display: none;
-  }
-
-  .chat {
-    margin-left: 0
-  }
-
-  .chat-history {
-    height: 300px;
-    overflow-x: auto
-  }
-}
-
-@media only screen and (min-width: 768px) {
-
-  .chat-history,
-  .chat-list {
-    height: 600px;
-    overflow-x: auto;
-  }
-
+@media (min-width: 768px) {
   .chat {
     border-left: 1px solid #eaeaea
   }
