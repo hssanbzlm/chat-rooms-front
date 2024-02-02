d88fa21181f06a4880a2d534a994ce7dcff6c6d2
Hssan Bouzlima <bouzlima.h@live.fr>
2024-02-02 13:36:26 +0100

    #20: Refactor chatView (#23)

--- src/composables/MessagesRequest.ts
+++ src/composables/MessagesRequest.ts
@@ -1,12 +1,10 @@
 import { useAxios } from '@vueuse/integrations/useAxios'
 import { getMethod, getMessagesUrl } from '@/api/requests'
-import { type message } from '@/interfaces/message'
 import { ref, watch } from 'vue'
 
-export function useMessages() {
+export function useMessagesRequest() {
   const list = ref(1)
   const { isLoading, error, data, execute } = useAxios()
-  const messages = ref<message[]>([])
   const nextMessageList = () => (list.value += list.value + 1)
 
   const loadMessages = async () => {
@@ -14,9 +12,6 @@ export function useMessages() {
       method: getMethod,
       withCredentials: true
     })
-    if (data.value) {
-      messages.value.push(...data.value.messages)
-    }
   }
   watch(
     list,
@@ -28,7 +23,7 @@ export function useMessages() {
 
   return {
     nextMessageList,
-    messages,
+    data,
     isLoading,
     error
   }
