f661b9451f8626ef36af7f8dc4beeff666cc4bf7
Hssan Bouzlima <bouzlima.h@live.fr>
2024-02-04 18:53:09 +0100

    #15 Fix authguard bug (#26)

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -2,19 +2,20 @@ import { router } from './route'
 import { useAxios } from '@vueuse/integrations/useAxios'
 import { isAuthUrl, getMethod } from '@/api/requests'
 const { execute } = useAxios()
-router.beforeEach(async (to, from) => {
+router.beforeEach(async (to, from, next) => {
   if (to.meta.requiresAuth) {
     try {
       const { data } = await execute(isAuthUrl, { method: getMethod, withCredentials: true })
-      if (data.value) return true
-      else return false
+      if (data.value) next()
+      else next({ name: 'join' })
     } catch (err) {
-      return { name: 'join' }
+      next({ name: 'join' })
     }
   } else {
-    if (from.fullPath == '/' && to.name != 'join') {
-      return { name: 'chat' }
-    }
+    if (to.redirectedFrom && to.redirectedFrom.name === 'chat') next()
+    else if (to.redirectedFrom === undefined && from.name === undefined) {
+      next({ name: 'chat' })
+    } else next()
   }
 })
 
