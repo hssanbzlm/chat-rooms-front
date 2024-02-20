17944b31c0dfd3b1dec335e0c9a26d84f7d42581
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-20 09:58:04 +0100

    #63 Search connected users

--- src/components/ChatConnectedList.vue
+++ src/components/ChatConnectedList.vue
@@ -1,5 +1,5 @@
 <template>
-    <li v-for="user in connectedUsersStore.connectedUsers" :key="user.userId"
+    <li v-for="user in props.connectedUsers" :key="user.userId"
         :class="['list-group-item p-2 float-start', { 'link': user.userId != userStore.user?.userId }]"
         @click="toPrivate(user.userId)">
         <img :src="user.avatar ? user.avatar : 'https://ui-avatars.com/api/?name=' + `${user.fullName}`" alt="avatar"
@@ -11,11 +11,10 @@
     </li>
 </template>
 <script setup lang="ts">
-import { useConnectedUsers } from '@/store/ConnectedUsers';
+import type { User } from '@/interfaces/user';
 import { useUser } from "@/store/User"
-const connectedUsersStore = useConnectedUsers()
 const userStore = useUser()
-
+const props = defineProps<{ connectedUsers: User[] }>()
 const toPrivate = (idUser: string) => {
     if (userStore.user?.userId !== idUser) {
         window.open(`chat/private/${idUser}`, '_blank')
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" @input="onSearch" placeholder="Search" v-model="text">
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+const emits = defineEmits(['search'])
+
+const onSearch = () => {
+    emits('search', text.value)
+}
+
+</script>
+<style></style>
