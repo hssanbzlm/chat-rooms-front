793431b9706499e3e602d5ef6b1ba5e97cb86705
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-21 09:17:18 +0100

    Specify socket transport

--- src/listeners/socket.ts
+++ src/listeners/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 const socketUrl = import.meta.env.VITE_API_URL
-const socket = io(socketUrl, { withCredentials: true })
+const socket = io(socketUrl, { withCredentials: true, transports: ['websocket'] })
 
 export default socket
