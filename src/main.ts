681775955901ffab20cdc243cbd0afe8f2b64f95
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-29 15:17:50 +0100

    Move pinia to plugins

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { FontAwesomeIcon } from '@/plugins/fa-icons'
-import { createPinia } from 'pinia'
+import { pinia } from '@/plugins/pinia'
 
-const pinia = createPinia()
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
